b5e0639396647e5b91a3e7d92909dfa8c876838e
Crystal Danger <cdneal091@gmail.com>
2020-04-17 16:01:51 -0400

    added basic express route

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const { Pool, Client } = require('pg')
+const express = require('express');
+const app = express()
+
 const connectionString = process.env.DATABASE_URL
 const pool = new Pool({
   connectionString: connectionString,
 })
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
+
+const query = (response) => {
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+    response.send(res)
+  })
+}
+
+
+app.get('/', (req, res) => {
+  query(res)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('started');
 })
